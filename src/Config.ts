bdd15d0564090e94ee980299b91af7fd511a19ae
James Black <james@james-black.com>
2026-08-24 02:14:19 -0500

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -14,4 +14,4 @@ export const ticksPerSecond = PLATFORM === 'mobile' ? 40 : 200
  * Multiplier applied to all simulated game time. 1 is normal speed.
  */
 export const gameSpeed = 1
-export const gameSpeed2 = 100
+export const gameSpeed2 = 1
